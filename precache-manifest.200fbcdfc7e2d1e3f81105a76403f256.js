3cb658c8e515adb43b8a37977ca4ad07c385bbb2
Yeswanth Raghav T <tyraghav@outlook.com>
2020-05-16 22:39:05 +0530

    Updates

---
+++ precache-manifest.200fbcdfc7e2d1e3f81105a76403f256.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87ae60cf341515e5a9e450ddb8bc9d7",
+    "url": "/reactburgerbuilder/docs/index.html"
+  },
+  {
+    "revision": "a66dc6e346c996bef0cc",
+    "url": "/reactburgerbuilder/docs/static/css/3.953727f5.chunk.css"
+  },
+  {
+    "revision": "615a99dd97fe170fb97a",
+    "url": "/reactburgerbuilder/docs/static/css/4.0f0bca46.chunk.css"
+  },
+  {
+    "revision": "6108069e35053a68e58f",
+    "url": "/reactburgerbuilder/docs/static/css/5.5feca9d6.chunk.css"
+  },
+  {
+    "revision": "8541d2a35985aa8e866e",
+    "url": "/reactburgerbuilder/docs/static/css/main.c285fe20.chunk.css"
+  },
+  {
+    "revision": "67546fd0bbd84713026b",
+    "url": "/reactburgerbuilder/docs/static/js/2.4dffcec5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactburgerbuilder/docs/static/js/2.4dffcec5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a66dc6e346c996bef0cc",
+    "url": "/reactburgerbuilder/docs/static/js/3.5189ad57.chunk.js"
+  },
+  {
+    "revision": "615a99dd97fe170fb97a",
+    "url": "/reactburgerbuilder/docs/static/js/4.ecbe916d.chunk.js"
+  },
+  {
+    "revision": "6108069e35053a68e58f",
+    "url": "/reactburgerbuilder/docs/static/js/5.b3dbd3ab.chunk.js"
+  },
+  {
+    "revision": "8541d2a35985aa8e866e",
+    "url": "/reactburgerbuilder/docs/static/js/main.2fdecfda.chunk.js"
+  },
+  {
+    "revision": "09bbc967d075100db9e5",
+    "url": "/reactburgerbuilder/docs/static/js/runtime-main.d2af4895.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/reactburgerbuilder/docs/static/media/burger-logo.b8503d26.png"
+  }
+]);
